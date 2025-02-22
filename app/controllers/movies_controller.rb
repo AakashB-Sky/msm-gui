2b9733a8753ab5d1c0ebf6b19ab30a12caafbd9b
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-22 19:18:08 +0000

    added action to add new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,18 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def insert
+    new_movie = Movie.new
+
+    new_movie.title = params.fetch("movie_title")
+    new_movie.year = params.fetch("movie_year")
+    new_movie.duration = params.fetch("movie_duration")
+    new_movie.description = params.fetch("movie_desc")
+    new_movie.image = params.fetch("movie_image_url")
+    new_movie.director_id = params.fetch("movie_director_id")
+    new_movie.save
+
+    redirect_to("/movies")
+  end
 end
